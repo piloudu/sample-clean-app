4655017c9709047300633e96cf39d63b5ffb0a6d
Xoan Gonzalez Iglesias <xoangon@gmail.com>
2023-04-03 20:43:08 +0200

    ADD: Empty Idea multiplatform project

---
+++ common/src/androidMain/kotlin/com/sample/clean/common/platform.kt
@@ -0,0 +1,5 @@
+package com.sample.clean.common
+
+actual fun getPlatformName(): String {
+    return "Android"
+}
---
+++ common/src/commonMain/kotlin/com/sample/clean/common/platform.kt
@@ -0,0 +1,3 @@
+package com.sample.clean.common
+
+expect fun getPlatformName(): String
